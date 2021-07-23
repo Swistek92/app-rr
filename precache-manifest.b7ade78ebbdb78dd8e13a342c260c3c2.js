bf4ea99f67e34c9db030900ac8a791746092c5dc
Swistek92 <swistekxD@gmail.com>
2021-07-23 14:56:08 +0200

    Updates

---
+++ precache-manifest.b7ade78ebbdb78dd8e13a342c260c3c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e61a1ee5a15d0a5352e",
+    "url": "/app-rr/static/css/main.adaa56bd.chunk.css"
+  },
+  {
+    "revision": "4e61a1ee5a15d0a5352e",
+    "url": "/app-rr/static/js/main.4e61a1ee.chunk.js"
+  },
+  {
+    "revision": "9873500cbc742282ea50",
+    "url": "/app-rr/static/js/runtime~main.9873500c.js"
+  },
+  {
+    "revision": "e12795637c47cc4e6448",
+    "url": "/app-rr/static/js/2.e1279563.chunk.js"
+  },
+  {
+    "revision": "ec79daccafa45315dbf18ff19e5f48fb",
+    "url": "/app-rr/static/media/header1.ec79dacc.jpg"
+  },
+  {
+    "revision": "da7832ec4d2c3c640df61d62dc4a9cf3",
+    "url": "/app-rr/static/media/header3.da7832ec.jpg"
+  },
+  {
+    "revision": "4063f34f8f36dbda9e9875759a66aa62",
+    "url": "/app-rr/static/media/header2.4063f34f.jpg"
+  },
+  {
+    "revision": "6a25b93a3cbfcf21dfdf3d33b48386f0",
+    "url": "/app-rr/index.html"
+  }
+];
